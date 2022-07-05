db9fa1f821006221c47ddb2f1f8e9aec61e983ae
gladyn <gladynwong@gmail.com>
2022-07-05 10:53:07 +0800

    edit post

--- screens/CreateScreen.js
+++ screens/CreateScreen.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 import { lightStyles, commonStyles } from "../styles/commonStyles";
+import axios from "axios";
+import { API, API_CREATE } from "../constants/API";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 export default function CreateScreen({ navigation }) {
@@ -10,9 +13,24 @@ export default function CreateScreen({ navigation }) {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
-  function savePost() {
-
+  async function savePost() {
+    const post = {
+      title: title,
+      content: content,
+    };
+    const token = await AsyncStorage.getItem("token");
+    try {
+      console.log(token);
+      const response = await axios.post(API + API_CREATE, post, {
+        headers: { Authorization: `JWT ${token}` },
+      });
+      console.log(response.data);
+      navigation.navigate("Index", { post: post });
+    } catch (error) {
+      console.log(error);
+    }
   }
+
 
   return (
     <View style={styles.container}>
